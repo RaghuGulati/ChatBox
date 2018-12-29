1fb01c7a511d022e0fdb909fbe102c46968d060d
RaghuGulati <45628486+RaghuGulati@users.noreply.github.com>
2018-12-29 10:43:16 +0530

    Update README.py

--- README.py
+++ README.py
@@ -9,14 +9,10 @@ conv = open('chats.txt','r').readlines()
 bot.set_trainer(ListTrainer)
 
 bot.train(conv)
-i=0
+
 while True:
     request = input('You: ')
     response = bot.get_response(request)
     
-    if(conv[i]=="Q"):
-        print("Sorry Ans entered")
-    else:
-        print('Bot: ',response)
+    print('Bot: ',response)
    
-    i=i+1
